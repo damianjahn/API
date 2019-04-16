4bb7ded31a5896a35c11a254566f633647b472e4
Damian Jahn <damian.jahn@icloud.com>
2019-04-16 13:13:28 +0200

    token added

--- api/roots/products.js
+++ api/roots/products.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const Product = require('../models/product')
 const multer = require('multer')
+const checkAuth = require('../check-auth')
 
 
 const storage = multer.diskStorage({
@@ -70,7 +71,7 @@ router.get('/', (req, res, next) => {
    })
 })
 
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
    console.log(req.file)
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -95,7 +96,7 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
     
 })
 
-router.get('/:productId', (req, res, next) => {
+router.get('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
     .exec()
@@ -136,7 +137,7 @@ router.patch('/:productId', (req, res, next) => {
 
 
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId
     Product.remove({_id: id})
     .exec()
--- api/roots/user.js
+++ api/roots/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
@@ -49,7 +50,29 @@ router.delete('/:userId', (req, res, next) => {
 })
 
 router.post('/login', (req, res, next) => {
-    
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length < 1){
+            return res.status(404).json({message: 'email doesnt exist'})
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, same) => {
+            if(err) {
+                return res.status(404).json({message: 'email doesnt exist'})
+            }
+            if(same) {
+                const token = jwt.sign({ email: user[0].email, userId: user[0]._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
+                res.status(200).json({message: 'ok', token: token})
+            }
+            else {
+                res.status(401).json({message: 'fail'})
+            }
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: err})
+    })
 })
 
 module.exports = router
